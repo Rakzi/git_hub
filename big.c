69e8ac09ba3b28376d44e16127670539677506df
Rakzi <49304882+Rakzi@users.noreply.github.com>
2019-04-11 23:01:57 +0530

    updated

--- big.c
+++ big.c
@@ -2,10 +2,8 @@
 big()
 {
     double n1, n2, n3;
-#this is big three
     printf("Enter three different numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
-#reciving changes from lava and implimenting on cetral hub which is rakzi hub
     if( n1>=n2 && n1>=n3 )
         printf("%.2f is the largest number.", n1);
 
